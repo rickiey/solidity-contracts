47faf9f7731a3ee3dff95a6c314700f82b51782a
rickiey <rickiey@qq.com>
2022-11-20 22:32:42 +0800

    add ganache

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 require('dotenv').config();
+require("@nomiclabs/hardhat-ganache");
 
 
 // Go to https://www.alchemyapi.io, sign up, create
@@ -12,13 +13,18 @@ const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
-console.log(ALCHEMY_API_KEY)
-console.log(GOERLI_PRIVATE_KEY)
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "ganache",
+
   networks: {
+    ganache: {
+      gasLimit: 6000000000,
+      defaultBalanceEther: 10,
+      url: "http://127.0.0.1:8545"
+    },
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
--- test/erc20_test.js
+++ test/erc20_test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
@@ -9,8 +10,16 @@ describe("Token contract", function () {
     const hardhatToken = await Token.deploy();
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    console.log(ownerBalance)
-    console.log(await hardhatToken.totalSupply())
+    console.log(owner.address);
+    console.log(ownerBalance);
+    console.log(await hardhatToken.totalSupply());
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+
+    // let w = new ethers.Wallet("0x882A4Dcc8457Bd9a2AB4a5E3EaeF7a4A74B00990");
+    let w = new ethers.Wallet("379a05d3166b455bd5bdad9ff4c224b221c098b2dba09f541d73ff081c9b0e58");
+    let re = await hardhatToken.transfer(w.address,1000000);
+    console.log(re);
+    let b2 = await hardhatToken.balanceOf(w.address);
+    expect(b2, 1000000); 
   });
 });
