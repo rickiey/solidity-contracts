6587bfc2a83a683f92acfa906a627e60a95107ce
rickiey <rickiey@qq.com>
2022-11-21 20:00:41 +0800

    add deploy script

---
+++ scripts/deploy_simple_transfer.js
@@ -0,0 +1,23 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const weiAmount = (await deployer.getBalance()).toString();
+
+    console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+
+    // make sure to replace the "GoofyGoober" reference with your own ERC-20 name!
+    const ST = await ethers.getContractFactory("SimpleTransfer");
+    const st = await ST.deploy();
+
+
+    console.log("contract address:", st.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
